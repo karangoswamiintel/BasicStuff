4e7d4aee345d089b9d2b49297052761fb483afb7
Karan Goswami <43630510+karangoswamikenz@users.noreply.github.com>
2018-10-17 11:22:53 -0700

    [Node] Removed Chaitanya's comment

--- Node.h
+++ Node.h
@@ -5,8 +5,7 @@
 class Node
 {
 public:
-    void *data;  //why declared as pointer? Is it because u can allocate any type of data type. How can you identify what type of data type is st
-                 //stored in this. do you wanna allocate generalised Node?
+    void *data;
     Node *next;
     Node();
     Node(void* _d);
